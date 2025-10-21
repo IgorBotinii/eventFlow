026797ef07093943b2602468a12590cb0b175af8
Igor Botini <igorbotinii@gmail.com>
2025-10-21 19:34:44 -0300

    Ajustando os arquivos dentro das pastas

--- public/js/login.js
+++ public/js/login.js
@@ -1,13 +1,34 @@
-document.getElementById("loginForm").addEventListener("submit", function (e) {
-e.preventDefault();
+document.getElementById('loginForm').addEventListener('submit', async (e) => {
+  e.preventDefault();
 
-const email = document.getElementById("email").value;
-const senha = document.getElementById("senha").value;
+  const email = document.getElementById('email').value.trim();
+  const senha = document.getElementById('senha').value.trim();
 
-if (email === "" || senha === "") {
-alert("Preencha todos os campos!");
-} else {
-alert("Login efetuado com sucesso!");
-window.location.href = ""; //colocar caminho apos efetuar login
-}
+  if (!email || !senha) {
+  alert("Preencha todos os campos")
+    return;
+  }
+
+  try {
+    const resposta = await fetch('http://45.89.30.194:3211/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email_user: email, senha })
+    });
+
+    const data = await resposta.json();
+
+    if (resposta.ok) {
+     
+      localStorage.setItem('usuario', JSON.stringify(data.user));
+        window.location.href = '/public/telainicial.html';
+
+    } else {
+    alert("Login incorreto, revise as informações")
+    }
+  } catch (error) {
+    console.error('Erro:', error);
+    mensagem.textContent = 'Erro ao conectar com o servidor.';
+    mensagem.style.color = 'red';
+  }
 });
